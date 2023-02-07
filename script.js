d24b65da017777e8b5b3f518444490ac410b07bd
Lucas Rocha <108117674+LukkazRocha@users.noreply.github.com>
2023-02-07 15:02:15 -0300

    preenchendo interface com numeros

--- script.js
+++ script.js
@@ -6,11 +6,45 @@ let lateral = document.querySelector('.d-1-right');
 let numeros = document.querySelector('.d-1-3');
 
 let etapaAtual = 0;
+let numero = '';
 
-function comecarEtapa() {}
+function comecarEtapa() {
+  let etapa = etapas[etapaAtual];
+
+  let numeroHtml = '';
+
+  for (let i = 0; i < etapa.numeros; i++) {
+    if (i === 0) {
+      numeroHtml += '<div class="numero pisca"></div>';
+    }
+    numeroHtml += '<div class="numero"></div>';
+  }
+
+  seuVotoPara.style.display = 'none';
+  cargo.innerHTML = etapa.titulo;
+  descricao.innerHTML = '';
+  aviso.style.display = 'none';
+  lateral.innerHTML = '';
+  numeros.innerHTML = numeroHtml;
+}
+
+function atualizaInterface() {
+  alert('Finalizou');
+}
 
 function clicou(n) {
-  alert('Clicou em ' + n);
+  let elNumero = document.querySelector('.numero.pisca');
+  if (elNumero !== null) {
+    elNumero.innerHTML = n;
+    numero = `${numero}${n}`;
+
+    elNumero.classList.remove('pisca');
+    if (elNumero.nextElementSibling !== null) {
+      elNumero.nextElementSibling.classList.add('pisca');
+    } else {
+      atualizaInterface();
+    }
+  }
 }
 
 function branco() {
@@ -22,3 +56,5 @@ function corrige() {
 function confirma() {
   alert('Clicou em CONFIRMA!');
 }
+
+comecarEtapa();
